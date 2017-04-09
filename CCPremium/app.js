388e489a44429c22a0422013cd7acb8845041461
mikedvs41 <michael.maliniak@dtdigital.com.au>
2017-04-09 22:20:10 +1000

    finished coding for CC premium

--- CCPremium/app.js
+++ CCPremium/app.js
@@ -6,35 +6,30 @@
         // All cats are individual objects
         data: [
             {
-                src: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg",
+                src: "http://placekitten.com/g/300/302",
                 name: "Mittens"
             },
             {
-                src: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg",
+                src: "http://placekitten.com/g/300/300",
                 name: "Smittens"
             },
                     {
-                src: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg",
+                src: "http://placekitten.com/g/300/301",
                 name: "Buttons"
             },
             {
-                src: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg",
+                src: "http://placekitten.com/g/300/303",
                 name: "Pancake"
             },
                     {
-                src: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg",
+                src: "http://placekitten.com/g/300/305",
                 name: "Flossy"
             },
             {
-                src: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg",
+                src: "http://placekitten.com/g/300/307",
                 name: "Garfield"
             }
         ],
-        updateCount: function(itemNumber) {
-            var currentCount = data[itemNumber].count;
-            currentCount++
-            return currentCount;
-        },
         initCount: function(array) {
             array.forEach(function(item, index){
                 item.count = 0;
@@ -46,6 +41,7 @@
             var data = controller.getAllData();
             model.initCount(data);
             viewButtons.init(data);
+            viewImg.render(0);
         },
         getAllData: function(){
             return model.data;
@@ -59,10 +55,19 @@
             });
         },
         attachImgEvent: function(number) {
-            console.log(number);
+            imgEl = document.querySelector('.img-current');
+            imgEl.addEventListener('click', function(){
+                controller.updateCount(number);
+            });
         },
         showImage: function(indexNumber) {
-            viewImg.render(indexNumber, controller.attachImgEvent(indexNumber));
+            viewImg.render(indexNumber);
+        },
+        updateCount: function(itemNumber) {
+            var currentCount = model.data[itemNumber].count;
+            currentCount++
+            model.data[itemNumber].count = currentCount;
+            viewImg.renderCount(currentCount);    
         }
     };
     var viewButtons = {
@@ -96,7 +101,9 @@
             
             title.innerText = obj.name;
             image.setAttribute('src', obj.src);
+            image.classList.add('img-current');
             count.innerText = obj.count;
+            count.classList.add('count-current');
 
             container.appendChild(title);
             container.appendChild(image);
@@ -104,12 +111,16 @@
 
             return container;
         },
-        render: function(item, callback){
+        render: function(item){
             var viewContainer = document.querySelector('.main-img-view');
             viewContainer.innerHTML = '';
             var renderItem = viewImg.createImage(item);
             viewContainer.appendChild(renderItem);
-            // callback(item);
+            controller.attachImgEvent(item);
+        },
+        renderCount: function(count) {
+            var countContainer = document.querySelector('.count-current');
+            countContainer.innerText = count;
         }
     };
     controller.init();
