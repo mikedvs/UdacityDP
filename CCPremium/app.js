3c056a4175b08711e5aa015c9daafe19a941ac40
Maliniak, Michael <Michael.Maliniak@news.com.au>
2017-04-11 17:28:58 +1000

    added final functionality for CC Premium

--- CCPremium/app.js
+++ CCPremium/app.js
@@ -2,8 +2,23 @@
 
 //IIFE
 (function(){
+
+	//DOM elements
+	var nameInput = document.getElementById('input-name'),
+		urlInput = document.getElementById('input-url'),
+		clickInput = document.getElementById('input-click'),
+		adminButton = document.querySelector('.admin-button'),
+		adminSubmit = document.querySelector('.admin-submit');
+		adminCancel = document.querySelector('.admin-cancel');
+
+	// Model - All data here, * only interacts with controller
+	// never with view directly *
     var model = {
-        // All cats are individual objects
+
+        // Current Cat is set to zero
+		currentCatIndex: 0,
+
+		// All cats in the form of individual objects
         data: [
             {
                 src: "http://placekitten.com/g/300/302",
@@ -30,27 +45,36 @@
                 name: "Garfield"
             }
         ],
+
+		// set the initial count of each cat to zero
         initCount: function(array) {
             array.forEach(function(item, index){
                 item.count = 0;
             });
         }
     };
+
+	// Controller - Between Model and View Items
     var controller = {
+
+		// initialize everything
         init: function(){
             var data = controller.getAllData();
             model.initCount(data);
             viewButtons.init(data);
-            viewImg.render(0);
+            viewImg.render(model.currentCatIndex);
+			viewAdmin.init();
         },
         getAllData: function(){
             return model.data;
         },
         attachListEvents: function() {
-            var elements = document.querySelectorAll('.list-button');
-            elements.forEach(function(el, index){
+            var listElements = document.querySelectorAll('.list-button');
+            listElements.forEach(function(el, index){
                 el.addEventListener('click',function(){
                     controller.showImage(index);
+					// Set the current cat to the last item clicked
+					model.currentCatIndex = index;
                 });
             });
         },
@@ -68,19 +92,54 @@
             currentCount++
             model.data[itemNumber].count = currentCount;
             viewImg.renderCount(currentCount);    
-        }
+        },
+		adminHideShow: function() {
+			var adminContent = document.querySelector('.admin-controls');
+			adminContent.classList.toggle('hidden');
+		},
+		adminSubmit: function(event) {
+			event.preventDefault();
+			var catObject = model.data[model.currentCatIndex],
+				updatedValues = controller.getInputValue();
+
+			catObject.name = updatedValues.name;
+			catObject.src = updatedValues.src;
+			catObject.count = updatedValues.count;
+			
+			viewImg.renderCount(catObject.count);
+			controller.updateNames(catObject.name);
+		},
+		adminCancel: function(event) {
+			event.preventDefault();
+			viewAdmin.render();
+			controller.adminHideShow();
+		},
+		getInputValue: function() {
+			var valueObject = {};
+			valueObject.name = nameInput.value;
+			valueObject.src = urlInput.value;
+			valueObject.count = clickInput.value;
+			return valueObject;
+		},
+		updateNames: function(name) {
+			var currentListButton = document.querySelector('.list-button-' + model.currentCatIndex),
+				currentTitle = document.querySelector('.title-current');
+			currentListButton.innerText = name;
+			currentTitle.innerText = name;
+		}
     };
     var viewButtons = {
-        createButton: function(obj) {
+        createButton: function(obj, index) {
             var button = document.createElement('button');
             button.innerText = obj.name;
             button.classList.add('list-button');
+			button.classList.add('list-button-' + index);
             return button;
         },
         render: function(buttons, callback){
             var viewContainer = document.querySelector('.main-list-view');
-            buttons.forEach(function(button){
-                var renderItem = viewButtons.createButton(button);
+            buttons.forEach(function(button, index){
+                var renderItem = viewButtons.createButton(button, index);
                 viewContainer.appendChild(renderItem);
             });
             callback();
@@ -100,6 +159,7 @@
                 count = document.createElement('p');
             
             title.innerText = obj.name;
+			title.classList.add('title-current');
             image.setAttribute('src', obj.src);
             image.classList.add('img-current');
             count.innerText = obj.count;
@@ -122,6 +182,27 @@
             var countContainer = document.querySelector('.count-current');
             countContainer.innerText = count;
         }
+
     };
+	// Admin controls view
+	var viewAdmin = {
+		// initialize all buttons once and once only
+		init: function() {
+			viewAdmin.render();
+			adminButton.addEventListener('click', controller.adminHideShow);
+			adminSubmit.addEventListener('click', controller.adminSubmit);
+			adminCancel.addEventListener('click', controller.adminCancel);
+		},
+		render: function() {
+
+			var catObject = model.data[model.currentCatIndex];
+			
+			nameInput.setAttribute('value', catObject.name);
+			urlInput.setAttribute('value', catObject.src);
+			clickInput.setAttribute('value', catObject.count);
+		}
+	}
+
+	// Starts here, initiates here
     controller.init();
 })();
